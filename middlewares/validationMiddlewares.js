b59e72e3d88aaaecc6409e7e996f6d5d54ed7228
dhilipanrobo <42709758+dhilipanrobo@users.noreply.github.com>
2024-06-10 17:20:45 +0530

    add validation name and password

--- middlewares/validationMiddlewares.js
+++ middlewares/validationMiddlewares.js
@@ -3,13 +3,13 @@ function vallidateSignup(req,res,next){
     const { email = "" ,name = "", password = "" } = req.body;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!name){
+    if (!name || username.length < 3 || username.length > 30){
         return res.status(400).json({status:400,message:'invalied name'})
     }
     if (!email || !emailRegex.test(email)){
         return res.status(400).json({status:400,message:'invalied Email'})
     }
-    if (!password){
+    if (!password || username.length < 6){
         return res.status(400).json({status:400,message:'invalied password'})
     }
     next()
